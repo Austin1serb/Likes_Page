8f27f78ba8a99e42c4485c330182888aabda1436
Austin Serb <128577470+Austin1serb@users.noreply.github.com>
2023-05-17 07:36:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var count = 3;
+var countElement = document.querySelector("#count");
+
+console.log(countElement);
+
+function add1() {
+count++
+countElement.innerText = count + " like(s)";
+console.log(count);
+}
+
+
+
+var count1 = 12;
+var countElement1 = document.querySelector("#count1");
+
+console.log(countElement1);
+
+function add2() {
+count1++
+countElement1.innerText = count1 + " like(s)";
+console.log(count1);
+}
+
+
+
+var count2 = 9;
+var countElement2 = document.querySelector("#count2");
+
+console.log(countElement2);
+
+function add3() {
+count2++
+countElement2.innerText = count2 + " like(s)";
+console.log(count2);
+}
